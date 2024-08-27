7f1aa3bb8d4d6919df5b678b23a0fbf676ee00bb
rodrigoaraujoh <164425390+rodrigoaraujoh@users.noreply.github.com>
2024-08-27 19:03:47 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,115 @@
+export const perguntas = [
+    {
+        enunciado: "Como é chamada a ação de impedir o ataque adversário diretamente sobre a rede?",
+        alternativas: [
+            {
+                texto: "a) Bloqueio",
+                afirmacao: [
+                    "a) Bloqueio"
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "b) Saque",
+                afirmacao: [
+                    "a) Bloqueio"
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é o máximo de toques permitidos por uma equipe antes de devolver a bola ao adversário?",
+        alternativas: [
+            {
+                texto: "a) 4",
+                afirmacao: [
+                    "b) 3",
+    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "b) 3",
+                afirmacao: [
+                    "b) 3"
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é a pontuação necessária para vencer um set, exceto no tie-break?",
+        alternativas: [
+            {
+                texto: "a) 21 pontos",
+                afirmacao: [
+                    "b) 25 pontos"
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "b) 25 pontos",
+                afirmacao: [
+                    "b) 25 pontos"
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: " Qual é o nome da posição que usa uma camisa de cor diferente e tem funções defensivas especiais?",
+        alternativas: [
+            {
+                texto: "a)Levantador",
+                afirmacao: [
+                    "b) Líbero"
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "b) Líbero",
+                afirmacao: [
+                    "b) Líbero"
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Quantos jogadores há em cada equipe de voleibol em uma partida oficial?",
+        alternativas: [
+            {
+                texto: "a) 7",
+                afirmacao: [
+                    "b)6"
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "b)6",
+                afirmacao: [
+                    "b)6"
+                ],
+                proxima: 8,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é a altura da rede em partidas masculinas de voleibol?",
+        alternativas: [
+            {
+                texto: "a)2,43 metros",
+                afirmacao: [
+                    "a)2,43 metros"
+                ],
+            },
+            {
+                texto: "b) 2,35 metros",
+                afirmacao: [
+                    "a)2,43 metros"
+                ],
+            },
+        ]
+    },
+];
